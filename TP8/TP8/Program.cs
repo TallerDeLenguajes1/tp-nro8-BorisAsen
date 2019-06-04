1c79216f9a5969c0cdb5c9b5be9f90904540d804
BorisAsen <borisyasen@gmail.com>
2019-06-04 04:57:46 -0300

    TP8 termina2

--- TP8/TP8/Program.cs
+++ TP8/TP8/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,18 +9,37 @@ namespace TP8
 {
     class Program
     {
+        const string FileName = "registros.csv";
+
         //MAIN
         static void Main(string[] args)
         {
             List<empleado> empleados = new List<empleado>();
-            Console.Write("Ingrese la cantidad de empleados a crear: ");
-            int cant = Convert.ToInt16(Console.ReadLine());
-            BuildEmployesList(empleados, cant);
-            Console.Clear();
 
-            Console.WriteLine("\nEmpleados de la Empresa: ");
+            Console.Write("Empleados guardados previamente: ");
+            EmployeesFromCsvToList(empleados);
             ShowEmployes(empleados);
 
+            Console.WriteLine("\nPresione una tecla para generar nuevos empleados");
+            Console.ReadKey();
+            Console.Clear();
+
+            Console.Write("Ingrese la cantidad de empleados a crear: ");
+            int cant = Convert.ToInt16(Console.ReadLine());
+            Console.Clear();
+            BuildEmployesList(empleados, cant);
+
+            Console.WriteLine("\nLista de empleados actualizada: ");
+            ShowEmployes(empleados);
+
+            Console.WriteLine("\nPresione una tecla para guardar la lista actualizada");
+            Console.ReadKey();
+
+            SaveEmployees(empleados,cant);
+            Console.WriteLine("\nLos Empleados se guardaron con exito");
+
+            BackUp(empleados);
+            Console.WriteLine("\nEl BackUp se realizo correctamente");
 
             Console.ReadKey();
         }
@@ -31,7 +51,77 @@ namespace TP8
         public enum genders { Mujer, Hombre, Otro };
         public enum positions { Auxiliar, Admnistrativo, Ingeniero, Especialista, Investigador }
 
+        //Funcion para cargar empleados desde el csv
+        public static List<empleado> EmployeesFromCsvToList(List<empleado> empleados)
+        {
+            string[] content = File.ReadAllLines(FileName);
+            foreach (string line in content)
+            {
+                string[] delimitedContent = line.Split(';');
+                string name = delimitedContent[0];
+                string surname = delimitedContent[1];
+                string maritalStatus = delimitedContent[2];
+                string gender = delimitedContent[3];
+                string position = delimitedContent[4];
+                DateTime birthDate = Convert.ToDateTime(delimitedContent[5]);
+                DateTime entryDate = Convert.ToDateTime(delimitedContent[6]);
+                double pay = Convert.ToDouble(delimitedContent[7]);
+                int childrens = rand.Next(10);
 
+                empleado new_emp = new empleado(name, surname, maritalStatus, gender, position, birthDate, entryDate, pay, childrens);
+                empleados.Add(new_emp);
+            }
+
+            return empleados;
+        }
+
+        //Funcion para guardar los empleados en el csv
+        public static void SaveEmployees(List<empleado> empleados, int cant)
+        {
+            string new_line;
+            StreamWriter CsvFile = File.AppendText(FileName);
+            new_line = "";
+            CsvFile.WriteLine(new_line);
+
+            for (int i = empleados.Count()-cant; i < empleados.Count(); i++)
+            {
+                new_line = empleados[i].name + ";" + empleados[i].surname + ";" + empleados[i].maritalStatus + ";" + empleados[i].gender + ";" + empleados[i].position + ";" + empleados[i].birthDate.ToString("dd/MM/yyyy") + ";" + empleados[i].entryDate.ToString("dd/MM/yyyy") + ";" + empleados[i].pay.ToString();
+                //Console.WriteLine(new_line);
+                CsvFile.WriteLine(new_line);
+            }
+
+            //foreach (empleado e in empleados)
+            //{
+            //    new_line = e.name + ";" + e.surname + ";" + e.maritalStatus + ";"+ e.gender + ";" + e.position + ";" +  e.birthDate.ToString("dd/MM/yyyy") + ";" + e.entryDate.ToString("dd/MM/yyyy") + ";" +  e.pay.ToString();
+            //    Console.WriteLine(new_line);
+            //    CsvFile.WriteLine(new_line);
+            //}
+            CsvFile.Close();
+        }
+
+
+        public static empleado LoadEmploye()
+        {
+            string name = Enum.GetName(typeof(names), rand.Next(6)); //del 0 al 5
+            string surname = Enum.GetName(typeof(surnames), rand.Next(6)); //del 0 al 5
+            string mS = Enum.GetName(typeof(maritalStatus), rand.Next(2)); //del 0 al 1
+            string gender = Enum.GetName(typeof(genders), rand.Next(3)); //del 0 al 2
+            string position = Enum.GetName(typeof(positions), rand.Next(5)); //del 0 
+            double pay = rand.Next(10, 50) * 1000; // de 10k a 50k
+
+            DateTime BD_from = new DateTime(1939, 1, 1);
+            DateTime BD_to = new DateTime(2001, 1, 1);
+            DateTime birthDate = RandomDate(BD_from, BD_to);
+
+            DateTime ED_from = birthDate.AddDays(365 * 18);
+            DateTime ED_to = DateTime.Today;
+            DateTime entryDate = RandomDate(ED_from, ED_to);
+            int childrens = rand.Next(10);
+
+            empleado new_emp = new empleado(name, surname, mS, gender, position, birthDate, entryDate, pay, childrens);
+
+            return new_emp;
+        }
         //Funcion para crear la lista de empleados
         public static List<empleado> BuildEmployesList(List<empleado> empleados, int cant)
         {
@@ -63,8 +153,9 @@ namespace TP8
             DateTime ED_from = birthDate.AddDays(365 * 18);
             DateTime ED_to = DateTime.Today;
             DateTime entryDate = RandomDate(ED_from, ED_to);
+            int childrens = rand.Next(10);
 
-            empleado new_emp = new empleado(name, surname, mS, gender, position, birthDate , entryDate, pay);
+            empleado new_emp = new empleado(name, surname, mS, gender, position, birthDate , entryDate, pay, childrens);
 
             return new_emp;
         }
@@ -89,146 +180,74 @@ namespace TP8
             }
         }
 
-        ////Estructura Empleado
-        //public struct empleado
-        //{
-        //    public string name;
-        //    public string surname;
-        //    public string maritalStatus;
-        //    public string gender;
-        //    public string position;
-        //    public DateTime birthdDate;
-        //    public DateTime entryDate;
-        //    public double pay;
-        //}
+        public static string SelectBackUpPath()
+        {
+            int i = 1;
+            string[] availableDisks = Environment.GetLogicalDrives();
+            Console.WriteLine();
+            foreach (string drive in availableDisks)
+            {
+                Console.WriteLine("   "+i+") "+drive);
+                i++;
+            }
 
-        ////Funcion para crear la lista de empleados
-        //public static List<empleado> BuildEmployesList(List<empleado> empleados, int cant)
-        //{
-        //    empleado emp;
-        //    for (int i = 0; i < cant; i++)
-        //    {
-        //        emp = NewEmploye();
-        //        empleados.Add(emp);
-        //    }
+            Console.Write("\nIngrese el numero correspondiente al disco elegido: ");
+            int nro = Convert.ToInt16(Console.ReadLine());
+            string selectedDirectory = @availableDisks[nro-1];
+            Console.WriteLine("\nOpcion seleccionada: " + selectedDirectory);
 
-        //    return empleados;
-        //}
+            return selectedDirectory;
+        }
 
-        ////Funcion que crea un empleado con valores aleatorios
-        //static public Random rand = new Random();//Rand publico
-        //public static empleado NewEmploye()
-        //{
-        //    empleado new_emp;
-        //    new_emp.name = Enum.GetName(typeof(names), rand.Next(6)); //del 0 al 5
-        //    new_emp.surname = Enum.GetName(typeof(surnames), rand.Next(6)); //del 0 al 5
-        //    new_emp.maritalStatus = Enum.GetName(typeof(maritalStatus), rand.Next(2)); //del 0 al 1
-        //    new_emp.gender = Enum.GetName(typeof(genders), rand.Next(3)); //del 0 al 2
-        //    new_emp.position = Enum.GetName(typeof(positions), rand.Next(5)); //del 0 
-        //    new_emp.pay = rand.Next(10, 50) * 1000; // de 10k a 50k
+        public static void BackUp(List<empleado> empleados)
+        {
+            Console.WriteLine("\nSe enlistaran los discos disponibles con el fin de realizar un BackUp de los empleados de la empresa");
 
-        //    DateTime BD_from = new DateTime(1939, 1, 1);
-        //    DateTime BD_to = new DateTime(2001, 1, 1);
-        //    new_emp.birthdDate = RandomDate(BD_from, BD_to);
+            string BackUpPath = SelectBackUpPath() + "\\BackUpAgenda";
 
-        //    DateTime ED_from = new_emp.birthdDate.AddDays(365 * 18);
-        //    DateTime ED_to = DateTime.Today;
-        //    new_emp.entryDate = RandomDate(ED_from, ED_to);
+            Console.WriteLine("\nRuta elegida: " + BackUpPath);
 
-        //    return new_emp;
-        //}
+            string fileNumber = "";
+            if (Directory.Exists(BackUpPath))
+            {
+                string[] availableFiles = Directory.GetFiles(BackUpPath);
+                fileNumber = Convert.ToString(availableFiles.Length);
+            }
 
+            if (!File.Exists(BackUpPath))
+            {
+                Directory.CreateDirectory(BackUpPath);
+                
+                string backUpContent;
+                if (fileNumber == "0")
+                {
+                    BackUpPath = BackUpPath + "\\backup.bk";
+                }
+                else
+                {
+                    BackUpPath = BackUpPath + "\\backup(" + fileNumber + ").bk";
+                }
 
-        ////Funcion para crear una fecha aleatoria
-        //public static DateTime RandomDate(DateTime from, DateTime to)
-        //{
-        //    int Days_Until_Today = (to - from).Days;
-        //    int Days_to_add = rand.Next(Days_Until_Today + 1);
-        //    DateTime new_date = from.AddDays(Days_to_add);
+                FileStream fs = File.Create(BackUpPath);
+                using (StreamWriter bk = new StreamWriter(fs))
+                {
+                    backUpContent = BackUpString(empleados);
+                    //Console.Write(backUpContent);
+                    bk.Close();
+                }
+            }
+        }
 
-        //    return new_date;
-        //}
+        public static string BackUpString(List<empleado> empleados)
+        {
+            string text = "";
+            foreach (empleado e in empleados)
+            {
+                text = text+ e.name + ";" + e.surname + ";" + e.maritalStatus + ";" + e.gender + ";" + e.position + ";" + e.birthDate.ToString("dd/MM/yyyy") + ";" + e.entryDate.ToString("dd/MM/yyyy") + ";" + e.pay.ToString() +"\n";
+            }
 
-
-        ////Funcion para calcular la edad
-        //public static int Age(DateTime birthDate)
-        //{
-        //    return DateTime.Today.Subtract(birthDate).Days / 365;
-        //}
-
-
-        ////Funcion para calcular los años de servicio o antiguedad
-        //public static int YearsOfService(DateTime entryDate)
-        //{
-        //    return DateTime.Today.Subtract(entryDate).Days / 365;
-        //}
-
-        //public static int YearsLeftToRetirement(int yearsOfService, string gender)
-        //{
-        //    switch (gender)
-        //    {
-        //        case "Mujer":
-        //            return (60 - yearsOfService);
-        //        case "Hombre":
-        //            return (65 - yearsOfService);
-        //        case "Otro":
-        //            return (62 - yearsOfService);
-        //        default:
-        //            return 0;
-        //    }
-        //}
-
-
-        ////Funcion para calcular el salario final
-        //public static double Salary(double pay, string maritalStatus, int childrens, int YearsOfService, string position)
-        //{
-        //    double aditional;
-        //    if (YearsOfService > 20)
-        //    {
-        //        aditional = pay * 0.25;
-        //    }
-        //    else
-        //    {
-        //        aditional = pay * 0.02 * YearsOfService;
-        //    }
-
-        //    if (position == "Ingeniero" || position == "Especialista")
-        //    {
-        //        aditional = aditional * 1.5;
-        //    }
-
-        //    if (maritalStatus == "Casado" && childrens > 3)
-        //    {
-        //        aditional = aditional + 5000;
-        //    }
-        //    return aditional + pay;
-        //}
-
-
-        ////Funcion para mostrar la lista de empleados
-        //public static void ShowEmployes(List<empleado> empleados)
-        //{
-        //    foreach (empleado e in empleados)
-        //    {
-        //        Console.WriteLine("\n\n* Empleado: ");
-        //        Console.WriteLine("    Nombre Completo: " + e.name + " " + e.surname);
-        //        Console.WriteLine("    Fecha de Nacimiento: " + e.birthdDate.ToString("dd/MM/yyyy"));
-        //        Console.WriteLine("    Fecha de Ingreso a la empresa: " + e.entryDate.ToString("dd/MM/yyyy"));
-        //        Console.WriteLine("    Estado Civil: " + e.maritalStatus);
-        //        Console.WriteLine("    Genero: " + e.gender);
-        //        Console.WriteLine("    Cargo: " + e.position);
-        //        Console.WriteLine("    Sueldo Base: $" + e.pay);
-
-        //        //Calculos adicionales del punto 2
-
-        //        Console.WriteLine("\n    Edad: " + Age(e.birthdDate) + " años");
-        //        Console.WriteLine("    Antiguedad: " + YearsOfService(e.entryDate) + " años");
-        //        Console.WriteLine("    Años faltantes para la jubilacion: " + YearsLeftToRetirement(YearsOfService(e.entryDate), e.gender) + " años");
-        //        int childrens = rand.Next(10);
-        //        Console.WriteLine("    Cantidad de hijos: " + childrens);
-        //        Console.WriteLine("    Salario: $" + Salary(e.pay, e.maritalStatus, childrens, YearsOfService(e.entryDate), e.position));
-        //    }
-        //}
+            return text;
+        }
 
     }
 }
--- TP8/TP8/empleado.cs
+++ TP8/TP8/empleado.cs
@@ -8,18 +8,20 @@ namespace TP8
 {
     class empleado
     {
-        private string name;
-        private string surname;
-        private string maritalStatus;
-        private string gender;
-        private string position;
-        private DateTime birthDate;
-        private DateTime entryDate;
-        private double pay;
+        public string name;
+        public string surname;
+        public string maritalStatus;
+        public string gender;
+        public string position;
+        public DateTime birthDate;
+        public DateTime entryDate;
+        public double pay;
+        public int childrens;
 
-        public empleado(string name, string surname, string maritalStatus, string gender, string position, DateTime birthDate, DateTime entryDate, double pay)
+        public Random rand = new Random();//Rand publico
+        public empleado(string name, string surname, string maritalStatus, string gender, string position, DateTime birthDate, DateTime entryDate, double pay, int childrens)
         {
-            Console.WriteLine("Objeto Empleado construido");
+            //Console.WriteLine("Objeto Empleado construido");
             this.name = name;
             this.surname = surname;
             this.maritalStatus = maritalStatus;
@@ -28,6 +30,7 @@ namespace TP8
             this.birthDate = birthDate;
             this.entryDate = entryDate;
             this.pay = pay;
+            this.childrens = childrens;
         }
 
         //Funcion para calcular la edad
@@ -59,10 +62,9 @@ namespace TP8
         }
 
         //Funcion para calcular el salario final
-        public Random rand = new Random();//Rand publico
+
         public double Salary()
         {
-            int childrens = rand.Next(10);
             double aditional;
             if (YearsOfService() > 20)
             {
@@ -100,12 +102,28 @@ namespace TP8
             Console.WriteLine("\n    Edad: " + Age() + " años");
             Console.WriteLine("    Antiguedad: " + YearsOfService() + " años");
             Console.WriteLine("    Años faltantes para la jubilacion: " + YearsLeftToRetirement() + " años");
-            int childrens = rand.Next(10);
             Console.WriteLine("    Cantidad de hijos: " + childrens);
             Console.WriteLine("    Salario: $" + Salary());
         }
 
+        public void ReturnEmployee()
+        {
+            Console.WriteLine("\n\n* Empleado: ");
+            Console.WriteLine("    Nombre Completo: " + name + " " + surname);
+            Console.WriteLine("    Fecha de Nacimiento: " + birthDate.ToString("dd/MM/yyyy"));
+            Console.WriteLine("    Fecha de Ingreso a la empresa: " + entryDate.ToString("dd/MM/yyyy"));
+            Console.WriteLine("    Estado Civil: " + maritalStatus);
+            Console.WriteLine("    Genero: " + gender);
+            Console.WriteLine("    Cargo: " + position);
+            Console.WriteLine("    Sueldo Base: $" + pay);
 
+            //Calculos adicionales del punto 2
+            Console.WriteLine("\n    Edad: " + Age() + " años");
+            Console.WriteLine("    Antiguedad: " + YearsOfService() + " años");
+            Console.WriteLine("    Años faltantes para la jubilacion: " + YearsLeftToRetirement() + " años");
+            Console.WriteLine("    Cantidad de hijos: " + childrens);
+            Console.WriteLine("    Salario: $" + Salary());
+        }
 
     }
 }
